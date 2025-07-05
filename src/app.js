746c4321a3fd4e3ad9171c2e51aeef80bf4ab234
abhinekpk <abhinekpk@gmail.com>
2025-07-05 18:21:08 +0530

    Learnt Routing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require("express") ;
 
 const app = express() ;
 
-app.use("/test" , (req,res)=>{
-    res.send("Hello from the Abhinek") ;
+app.get("/user" , (req,res)=>{
+    res.send({Name: "Abhinek" , lastNmae : "Pandey"}) ;
 }) ;
+
+app.post("/user" , (req,res)=>{
+    res.send("Data succesfully saved") ;
+}) ;
+
 
 app.listen(3000 , ()=>{
     console.log("server running on port 3000 of Abhinek") ;
