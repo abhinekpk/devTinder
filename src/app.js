5ea31331ef191642d960107b574b75534a25c6d5
abhinekpk <abhinekpk@gmail.com>
2025-07-06 08:09:52 +0530

    multipule - route handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,27 @@ const express = require("express") ;
 
 const app = express() ;
 
-app.get("/user" , (req,res)=>{
-    res.send({Name: "Abhinek" , lastNmae : "Pandey"}) ;
-}) ;
+app.get("/user" , 
+    (req,res,next)=>{
+        console.log("handling 1st route") ;
+        //res.send("!!Respose") ;
+        next() ;
+    },
+    (req,res,next)=>{
+        console.log("handling 2nd route") ;
+        //res.send("2nd !!Respose") ;
+        next() ;
+    },
+    (req,res,next)=>{
+        console.log("handling 3rd route") ;
+        //res.send("3rd !!Respose") ;
+        next() ;
+    },
+    (req,res,next)=>{
+        console.log("handling 4th route") ;
+        res.send("4th !!Respose") ;
+    },
+) ;
 
 app.post("/user" , (req,res)=>{
     res.send("Data succesfully saved") ;
