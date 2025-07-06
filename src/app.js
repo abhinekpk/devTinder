5216cd210150643e6bfbdce613dae181059d1d48
abhinekpk <abhinekpk@gmail.com>
2025-07-06 19:43:08 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ const { adminAuth , userAuth } =require("./middlewares/auth.js") ;
 
 const app = express() ;
 
-app.use("/admin",adminAuth) ;
-
 app.get("/admin",(req,res,next)=>{
+    throw new Error("Some problem") ;
     res.send("User data Sent") ;
 });
+
+app.use("/", (err , req, res , next)=>{
+    if(err){
+        res.status(500).send("Something went wrong") ;
+    }
+}) ;
 
 app.listen(3000 , ()=>{
     console.log("server running on port 3000 of Abhinek") ;
