d1f519cb0bed761c4092c9caf7c4189ebd2d0065
abhinekpk <abhinekpk@gmail.com>
2025-07-07 09:51:28 +0530

    Get , Delete And Update APIs

--- src/app.js
+++ src/app.js
@@ -21,12 +21,73 @@ app.post("/signup" , async (req , res ,next) =>{
     }
 } );
 
-connectDB().then(() => {
+app.patch("/user" , async (req,res,next) =>{
+    try{
+        await userModel.findByIdAndUpdate(req.body.userId,req.body) ;
+        res.send("User data succesfully updated" ) ;
+    }
+    catch{
+        res.status(400).send("Something went wrong") ;  
+    }
+}) ;
+
+app.get("/user" , async (req , res ,next) =>{
+    
+    try{
+        const user = await userModel.findOne({emailId : req.body.emailId }) ;
+        if(!user){
+            res.status(404).send("User not found") ;
+        }
+        else{
+            console.log("User Succefully found") ;
+            res.send(user) ;
+        }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong") ;
+    }
+    
+} );
+
+app.get("/feed" , async (req , res ,next) =>{
+    
+    try{
+        const user = await userModel.find({}) ;
+        if(!user){
+            res.status(404).send("User not found") ;
+        }
+        else{
+            console.log("User Succefully found") ;
+            res.send(user) ;
+        }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong") ;
+    }
+    
+} );
+
+app.delete("/user" , async (req , res ,next) =>{
+    
+    try{
+        const user = await userModel.findByIdAndDelete(req.body.userId) ;
+        res.send("User deleted succefully") ;
+    }
+    catch(err){
+        res.status(400).send("Something went wrong") ;
+    }
+    
+} );
+
+
+connectDB()
+.then(() => {
     console.log("Database connection established ... ") ;
     app.listen(3000 , ()=>{
         console.log("server running on port 3000 of Abhinek") ;
     }) ;
-}).catch( err => {
+})
+.catch( err => {
     console.error("Database connection can't be established ... ") ;
 })
 
