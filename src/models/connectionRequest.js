4ca9f2db033160ff9bc8e63ab73ce753caf9fff9
abhinekpk <abhinekpk@gmail.com>
2025-07-13 16:07:53 +0530

    API connection request accept/reject

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,38 +1,48 @@
-const mongoose = require('mongoose') ;
+const mongoose = require("mongoose");
 
-const connectionRequestSchema = mongoose.Schema({
-    fromUserId :{
-        type : mongoose.Schema.Types.ObjectId ,
-        required : true ,
-    } ,
-    toUserId : {
-        type : mongoose.Schema.Types.ObjectId ,
-        required : true ,
-    } ,
-    status : {
-        type : String ,
-        required : true ,
-        enum : {
-            values : ["interested" ,"ignored" , "accepted" , "rejected" ] ,
-            message : `{VALUE} is not a valid field` ,
-        }
-    }
-},
-{
-    timestamps : true 
-})
+const connectionRequestSchema = mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["interested", "ignored", "accepted", "rejected"],
+        message: `{VALUE} is not a valid field`,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+connectionRequestSchema.index({
+  fromUserId: 1,
+  toUserId: 1,
+});
 
 connectionRequestSchema.pre("save", function (next) {
-    const connectionRequest = this;
-    // Check if the fromUserId is same as toUserId
-    if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
-      throw new Error("Cannot send connection request to yourself!");
-    }
-    next();
-  });
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
 
-const ConnectionRequest = mongoose.model("ConnectionRequest",connectionRequestSchema) ;
+const ConnectionRequest = mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
 
-module.exports= {
-    ConnectionRequest ,
-} ;
+module.exports = {
+  ConnectionRequest,
+};
